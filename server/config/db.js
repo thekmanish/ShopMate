3edfd6615bb724f5b4733fd92d77a66d6892bfa2
thekmanish <itsksmanish@gmail.com>
2025-03-05 09:02:16 +0530

    debugged and completed server

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 async function connectDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connection passed : ${conn.connection.host}`);
   } catch (error) {
     console.log(`MongoDB connection failed : ${error.message}`);
     process.exit(1);
   }
 }
+
 export default connectDB;
